d86bf743d7f315c91ffb6c54a61fcedf0f40b9c2
ccwittv <ccwittv@yahoo.com.ar>
2016-11-07 17:03:00 -0300

    - Mejors Varias

--- scripts/services/factoryRutas.js
+++ scripts/services/factoryRutas.js
@@ -5,4 +5,8 @@ angular
     objeto.nombre = "Factory de Rutas";
     objeto.ApiBanderas = "http://www.egos27.somee.com/api/bandera";
     return objeto;
+
+// Se puede hacer una función de solo lectura (get) para recuperara la ruta
+// porque de esta manera los atrubutos de objeto son de lectura y escritura.
+
   })//Cierra factory
